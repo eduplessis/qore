e6007f0821075778af1f936d359091b22cbc5ca3
eduplessis <eduplessis@gmail.com>
2012-08-30 14:12:05 -0400

    Integration de WPML

--- library/theme-functions.php
+++ library/theme-functions.php
@@ -10,9 +10,10 @@ add_action( 'after_setup_theme', 'qore_hooks' );
 
 add_action( 'after_setup_theme', 'qore_filters' );
 
-add_action( 'template_redirect', 'qore_enqueues' , 10);
+add_action( 'template_redirect', 'qore_enqueues' , 9);
 
 add_action( 'admin_init', 'qore_version_init' );
+add_action()
 
 function qore_version_init() {
 
@@ -23,6 +24,7 @@ function qore_version_init() {
 
 
 }
+
 
 function qore_constants() {
 
@@ -41,7 +43,9 @@ function qore_constants() {
 }
 
 function qore_extensions() {
-	return FALSE;
+	if(function_exists('icl_get_home_url'){
+		require_once ( QORE_LIBRARY_ROOT . 'extensions/wpml-integration.php');
+	}
 }
 
 function qore_functions() {
