dbce9a6e60d68910682bd8fa8c3e89d2b6cd6876
eduplessis <eduplessis@gmail.com>
2012-09-13 10:49:59 -0400

    modification des class pour les nav

--- navigation.php
+++ navigation.php
@@ -1,6 +1,6 @@
 <?php if ( $wp_query->max_num_pages > 1 ) : ?>
 	<nav id="nav-above">
-		<div class="nav-previous"><?php next_posts_link( __( 'Suivant', 'qore' ) ); ?></div>
-		<div class="nav-next"><?php previous_posts_link( __( 'Précédent', 'qore' ) ); ?></div>
+		<div class="nav-next"><?php next_posts_link( __( 'Suivant', 'qore' ) ); ?></div>
+		<div class="nav-previous"><?php previous_posts_link( __( 'Précédent', 'qore' ) ); ?></div>
 	</nav><!-- #nav-above -->
 <?php endif; ?>
