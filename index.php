b6c9c1b92b9f480796773639dcbd2ff53a7b9dee
eduplessis <eduplessis@gmail.com>
2012-10-17 13:54:09 -0400

    automatic content-custom-post-type

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ get_header(); ?>
 
 				<?php while ( have_posts() ) : the_post(); ?>
 
-					<?php get_template_part( 'content'); ?>
+					<?php get_template_part( 'content', '' . get_post_type() . ''); ?>
 
 				<?php endwhile; ?>
 
