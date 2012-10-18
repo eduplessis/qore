895f9e40540e15e69b330c5da016e021fbaaf012
eduplessis <eduplessis@gmail.com>
2012-10-18 12:54:14 -0400

    archive

---
+++ archive.php
@@ -0,0 +1,23 @@
+<?php
+
+get_header(); ?>
+
+		<div id="primary">
+			<?php qore_begining_section() ?>
+			<div id="content" role="main">
+				<?php if (condition): ?>
+
+				<h1 class="archive-title"><?php single_term_title( $prefix, $display ); ?> </h1>
+
+					<?php while ( have_posts() ) : the_post(); ?>
+
+						<?php get_template_part( 'content', '' . get_post_type() . ''); ?>
+
+					<?php endwhile; ?>
+				<?php endif ?>
+			</div><!-- #content -->
+			<?php qore_closing_section() ?>
+		</div><!-- #primary -->
+
+
+<?php get_footer(); ?>
