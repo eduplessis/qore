84380528a5c8f495da93c975daac93f7087ea4bc
eduplessis <eduplessis@gmail.com>
2012-10-22 14:05:07 -0400

    Ajout de class pour les ancestors

--- library/functions/qore-cleanup.php
+++ library/functions/qore-cleanup.php
@@ -389,12 +389,21 @@ function qore_clean_style_tag($input) {
 
 
 function qore_body_class( $class ) {
+  global $post;
+  $parents =  get_ancestors($post->ID, get_post_type());
+
+  if ($parents) {
+    $class = array_merge( $class, array( 'ancestor-' . end($parents) . '' ) );
+  }
+
   if (!is_front_page()) {
-    $class = array_merge($class, array('not-front'));
+    $class = array_merge( $class, array('not-front'));
   }
   if (get_post_type()) {
     $class = array_merge($class, array(get_post_type()));
   }
+
+
   return $class;
 }
 add_filter( 'body_class', 'qore_body_class' );
