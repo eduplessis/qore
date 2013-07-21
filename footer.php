0c37ee94cb382b199146330d5844e62212859df3
Edouard Duplessis <eduplessis@gmail.com>
2013-07-21 12:32:39 -0400

    html5 please
    Changing for the element footer

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
 	<?php qore_before_footer()?>
-	<div id="footer">
+	<footer id="footer">
 		<?php echo get_qore_footerMenu()?>
 		<?php qore_inside_footer()?>
-	</div>
+	</footer>
 	<?php qore_after_footer()?>
 </div>
 <?php qore_after_container()?>
